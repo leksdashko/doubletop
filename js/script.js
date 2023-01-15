96615d7ac1903ad83438b1eeca001b1fb4cbe01f
Oleksandr Dashko <leksdashko@gmail.com>
2023-01-14 23:17:52 -0800

    smoother updated

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ const smoother = ScrollSmoother.create({
   content: '#smooth-content',
   smooth: 1.5,
   effects: true,
+  normalizeScroll: true,
   ignoreMobileResize: true,
   smoothTouch: 0.1,
 });
